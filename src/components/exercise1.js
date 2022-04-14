f4708539f04b1d517056d5666f236121aca9d70d
DevlopFE <jcxavidazn@gmail.com>
2022-04-14 22:59:24 +0200

    creating component exercises

---
+++ src/components/exercise1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const RangeFixed = () => {
+  return (
+    <div className='container'>
+      <header>RangeFixed</header>
+      <main>RangeFixedContainer</main>
+    </div>
+  )
+}
---
+++ src/components/exercise2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const RangeFree = () => {
+  return (
+    <div className='container'>
+      <header>RangeFree</header>
+      <main>RangeFreeContainer</main>
+  </div>
+  )
+}
