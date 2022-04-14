d01f74150aef8ac7b0de139f69a422ace27c3905
DevlopFE <jcxavidazn@gmail.com>
2022-04-14 22:59:04 +0200

    creating navigator component and routes

---
+++ src/components/navigator.js
@@ -0,0 +1,38 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  NavLink
+} from 'react-router-dom';
+
+import { RangeFixed } from './exercise1';
+import { RangeFree } from './exercise2';
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+            <div className='title'>Range</div>
+          <ul>
+            <li>
+              <NavLink to="/exercise1" className={({isActive}) => isActive ? "nav-active": ''} >Exercise 1</NavLink>
+            </li>
+            <li>
+              <NavLink to="/exercise2"  className={({isActive}) => isActive ? "nav-active": ''} >Exercise 2</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Navigate to='/exercise1' replace />} />
+          <Route path="/exercise1" element={<RangeFixed />} />
+          <Route path="/exercise2" element={<RangeFree />} />
+
+          <Route path="/*" element={<Navigate to='/exercise1' replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
