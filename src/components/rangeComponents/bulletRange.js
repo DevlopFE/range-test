8fae05d0ac5de5f4bb9e4400e820abe7f263e4b2
DevlopFE <jcxavidazn@gmail.com>
2022-04-15 17:34:24 +0200

    is it possible to add diferent max value

--- src/components/rangeComponents/bulletRange.js
+++ src/components/rangeComponents/bulletRange.js
@@ -1,11 +1,20 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useCallback}  from 'react'
 
-export const BulletRange = React.forwardRef( ({bulletRef, sign, position, ...props}) => {
+export const BulletRange = React.forwardRef( ({bulletRef, sign, position, minValue, maxValue, limit, ...props}) => {
+  
+  const getPositionFromValue = useCallback( (value) => {
+    const diffMaxMin = maxValue - minValue
+    const diffValMin = value - minValue
+    const percentage = diffValMin / diffMaxMin
+    const pos = Math.round(percentage * limit)
+    return pos
+  }, [maxValue, minValue, limit]);
+
   useEffect(() => {
     if(bulletRef.current){
-      bulletRef.current.style.left = `${position}px`;
+      bulletRef.current.style.left = `${getPositionFromValue(position)}px`;
     }
-  }, [bulletRef, position]);
+  }, [bulletRef, getPositionFromValue, position]);
 
   return (
     <div ref={bulletRef} {...props} />
