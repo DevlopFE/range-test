e6afb6155a01a07a97edc39a0ea4b05cdfcef158
DevlopFE <jcxavidazn@gmail.com>
2022-04-16 17:59:26 +0200

    readapting component

--- src/components/rangeComponents/inputRange.js
+++ src/components/rangeComponents/inputRange.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-export const InputRange = React.forwardRef( ({inputRef, onMouseChange,handleKeyPress, position, ...props}) => {
+export const InputRange = React.forwardRef( ({inputRef, position, ...props}) => {
   return (
     <input 
       // type="number"
       ref={inputRef}
-      onChange={onMouseChange}
-      onKeyPress={handleKeyPress}
       value={position}
       {...props}
+      readOnly
     />  
   )
 })
