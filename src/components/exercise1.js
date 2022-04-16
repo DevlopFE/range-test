ad1dcb929ff3b5d1283582e060e70646f2632b56
DevlopFE <jcxavidazn@gmail.com>
2022-04-16 17:59:44 +0200

    adding component on exercise1

--- src/components/exercise1.js
+++ src/components/exercise1.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { FixedRange } from './rangeComponents/fixedRange'
+import { BASE_URL } from '../config'
+import { useFetch } from './hooks/useFetch';
+import { Loader } from '../commons/loader';
 
 export const RangeFixed = () => {
+  const URL = `${BASE_URL}/fixed-range`
+  const {isLoading, response, error } = useFetch(URL)
+const VALUES_DEFAULT = [0,25,50,100, 200, 299]
   return (
     <div className='container'>
-      <header>Range Fixed</header>
+      <header>Fixed range</header>
       <main>
-       
+       {isLoading && <Loader />}
+       {error && <p>{error}</p>}
+        {response && <FixedRange values={response?.values || VALUES_DEFAULT} />}
       </main>
-    </div>
+  </div>
   )
 }
