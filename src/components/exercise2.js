052d7712754734e4bf774a097a43e4cd9abdc084
DevlopFE <jcxavidazn@gmail.com>
2022-04-16 17:15:01 +0200

    handling page depending on data

--- src/components/exercise2.js
+++ src/components/exercise2.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { MultiRange } from './rangeComponents/multiRange'
+import { BASE_URL } from '../config'
+import { useFetch } from './hooks/useFetch';
+import { Loader } from '../commons/loader';
 
 export const RangeFree = () => {
+  const URL = `${BASE_URL}/normal-range`
+  const {isLoading, response, error } = useFetch(URL)
+
   return (
     <div className='container'>
-      <header>Range</header>
+      <header>Normal range</header>
       <main>
-        <MultiRange minValue={0} maxValue={300} />
+        {isLoading && <Loader />}
+        {error && <p>{error}</p>}
+        {response && <MultiRange minValue={response.min} maxValue={response.max} />}
       </main>
   </div>
   )
