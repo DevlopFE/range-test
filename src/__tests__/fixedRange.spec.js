375fab335461db14ab650074675aec4c00874c08
DevlopFE <jcxavidazn@gmail.com>
2022-04-17 10:50:14 +0200

    removing old range components

--- src/__tests__/fixedRange.spec.js
+++
@@ -1,8 +0,0 @@
-import {FixedRange} from '../components/rangeComponents/fixedRange' 
-
-describe('FixedRange', () => {
-  it('should be defined', () => {
-    expect(FixedRange).toBeDefined();
-  });
-}
-);
--- src/__tests__/multiRange.spec.js
+++
@@ -1,8 +0,0 @@
-import {MultiRange} from '../components/rangeComponents/multiRange' 
-
-describe('MultiRange', () => {
-  it('should be defined', () => {
-    expect(MultiRange).toBeDefined();
-  });
-}
-);
